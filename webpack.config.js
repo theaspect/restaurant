aa302424fa705aad0c1475ba5c717b512647b1e0
Constantine Linnick <mail@blzr.me>
2022-11-09 07:04:13 +0700

    Broken dependency management

--- webpack.config.js
+++ webpack.config.js
@@ -2,21 +2,37 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    context: path.resolve(__dirname),
     mode: 'development',
-    entry: './src/index.js',
-    "devServer": {
-        "static": './dist',
+    entry: {
+        main: {
+            import: './site/main.js',
+            filename: "index.js",
+        },
+        echo1: {
+            import: './echo1/site/echo1.js',
+        }
     },
-    devtool: 'inline-source-map',
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'src/index.html'
-        }),
-    ],
     output: {
-        filename: '[name].js',
+        filename: '[name]/index.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    "devServer": {
+        "static": path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'site/index.html',
+            filename: 'index.html',
+            chunks: ['main'],
+        }),
+        new HtmlWebpackPlugin({
+            template: 'echo1/site/index.html',
+            filename: 'echo1/index.html',
+            chunks: ['echo1']
+        }),
+    ],
+    devtool: 'inline-source-map',
     optimization: {
         runtimeChunk: 'single',
     },
